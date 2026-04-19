c6890b2c396e1bb7288e81f6affdfc858233e8fe
binu2002 <binojmadhuranga2002.04.04@gmail.com>
2026-04-19 06:59:51 +0530

    Implement database seeding on application startup and set default values for BloodRequest properties

--- BloodDonationManagementSystem/Models/BloodRequest.cs
+++ BloodDonationManagementSystem/Models/BloodRequest.cs
@@ -3,8 +3,8 @@ namespace BloodDonationManagementSystem.Models;
 public class BloodRequest
 {
     public int Id { get; set; }
-    public string BloodGroup { get; set; }
+    public string BloodGroup { get; set; } = "";
     public int Quantity { get; set; }
-    public string Status { get; set; }
+    public string Status { get; set; } = "";
     public int HospitalId { get; set; }
 }
