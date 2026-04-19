6ec7137789d510f0c4629b1892a56b8049793b4c
binu2002 <binojmadhuranga2002.04.04@gmail.com>
2026-04-19 06:56:24 +0530

    Add AppDbContext and BloodRequest model for database integration

---
+++ BloodDonationManagementSystem/Models/BloodRequest.cs
@@ -0,0 +1,10 @@
+namespace BloodDonationManagementSystem.Models;
+
+public class BloodRequest
+{
+    public int Id { get; set; }
+    public string BloodGroup { get; set; }
+    public int Quantity { get; set; }
+    public string Status { get; set; }
+    public int HospitalId { get; set; }
+}
